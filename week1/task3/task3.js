b21db553a86c7a8843a9036eab4e855529767399
seymurDev <seymur2519@gmail.com>
2024-04-26 15:56:43 +0400

    task3 rewiew

--- week1/task3/task3.js
+++ week1/task3/task3.js
@@ -1,15 +1,34 @@
 const firstDate=document.getElementById('firstDate')
 const secondDate=document.getElementById('secondDate')
 const resultSpan=document.getElementById('result')
+function validateDate() {
+    var dateInput1 = firstDate.value;
+    var dateInput2 = secondDate.value;
+    var dateRegex = /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(19|20)\d{2}$/;
+    if (dateRegex.test(dateInput1) & dateRegex.test(dateInput2)) {
+        document.getElementById("validationMessage").innerText = "Valid date format.";
+        return true
+    } else {
+        document.getElementById("validationMessage").innerText = "Invalid date format. Please enter DD.MM.YYYY format.";
+        return false
+    }
+}
 function calculateDate(){
-
-    let firstDateObj = new Date(firstDate.value);
-    let secondDateObj = new Date(secondDate.value);
-    const timeDiff = Math.abs(secondDateObj.getTime() - firstDateObj.getTime());
-    console.log(timeDiff);
-    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    const months = Math.floor(days / 30);
-    const years = Math.floor(months / 12);
-    resultSpan.innerHTML =' ' + years + " year, " + months % 12 + " month, " + days % 30 + " day";
-
+    if(validateDate()){
+        var firstDatePart=firstDate.value.split('.')
+        var secondDatePart=secondDate.value.split('.')
+        var startDate = new Date(firstDatePart[2],firstDatePart[1] - 1,firstDatePart[0])
+        var endDate = new Date(secondDatePart[2],secondDatePart[1] - 1,secondDatePart[0]);
+        var timeDiff = Math.abs(endDate.getTime() - startDate.getTime()) / 1000;
+        console.log(timeDiff);
+        var years = Math.floor(timeDiff / (60 * 60 * 24 * 365));
+        timeDiff -= years * 60 * 60 * 24 * 365;
+        var months = Math.floor((timeDiff % (60 * 60 * 24 * 365)) / (60 * 60 * 24 * 30.44));
+        timeDiff -= months * 60 * 60 * 24 * 30.44;
+        var days = Math.floor((timeDiff % (60 * 60 * 24 * 365)) / (60 * 60 * 24));
+        resultSpan.textContent=`${years} il, ${months} ay, ${days} gün`
+    }
+    else{
+        return false
+    }
 }
