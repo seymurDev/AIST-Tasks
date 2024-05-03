01ddb3dcf74b72909b3efde69ab1c0becc7e1bda
seymurDev <seymur2519@gmail.com>
2024-05-03 09:37:00 +0400

    task4

---
+++ week2/task4/task4.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("responsive").style.width = "80%";
+  }
+  
+  function closeNav() {
+    document.getElementById("responsive").style.width = "0%";
+  }
