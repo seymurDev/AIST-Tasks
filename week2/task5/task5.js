aa86d171d30a0a2e476bbb484d924bc2cc5e2ba7
seymurDev <seymur2519@gmail.com>
2024-05-03 12:48:16 +0400

    task5

--- week2/task5/task5.js
+++ week2/task5/task5.js
@@ -1,14 +1,21 @@
 const prevBtn=document.getElementById('prev')
 const nextBtn=document.getElementById('next')
-const img=document.getElementById('img').src
-var imgCount=1
-console.log(img);
-prevBtn.addEventListener('click',prevAct())
+const img=document.getElementById('img')
+let imgCount=1
+
 function prevAct(){
     if(imgCount===1){
         imgCount=6
     }else{
-        imgCount-1
-        img=`./images/${imgCount}.jpg`
+        imgCount -=1
+        img.innerHTML=`<img src="./images/${imgCount}.jpg"  height="400px" alt="img">`
+    }
+}
+function nextAct(){
+    if(imgCount===6){
+        imgCount=1
+    }else{
+        imgCount +=1
+        img.innerHTML=`<img src="./images/${imgCount}.jpg"  height="400px" alt="img">`
     }
 }
